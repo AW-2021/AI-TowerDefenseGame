6548680416e847a2397cc4e0b6c061e0d19fd1c3
Amina Wasif <aminawasif20@gmail.com>
2023-04-16 17:18:53 +0500

    Created game.py

---
+++ game.py
@@ -0,0 +1,46 @@
+import pygame
+import os
+
+class Game:
+    def __init__(self):
+        self.width = 1300
+        self.height = 700
+        self.win = pygame.display.set_mode((self.width, self.height))
+        self.enemys = []
+        self.towers = []
+        self.lives = 10
+        self.money = 100
+        self.bg = pygame.image.load(os.path.join("../assets/backgrounds", "game_background_3.png"))
+        self.bg = pygame.transform.scale(self.bg, (self.width, self.height))
+        # self.clicks = []
+
+    def run(self):
+        run = True
+        clock = pygame.time.Clock()
+
+        while run:
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+
+                pos = pygame.mouse.get_pos()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    # self.clicks.append(pos)
+                    # print(self.clicks)
+                    pass
+
+            self.draw()
+
+        pygame.quit()
+
+    def draw(self):
+        self.win.blit(self.bg, (0,0))
+        # for p in self.clicks:
+            ## draw.circle => (rendering surface, colour, position(x, y), circle radius, fill or not)
+            # pygame.draw.circle(self.win, (255,0,0), (p[0], p[1]), 5, 0)                    
+        pygame.display.update()
+
+g = Game()
+g.run()
