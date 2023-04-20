a631b1b03f4a255ebe424f6529ed8178fd14850c
Amina Wasif <aminawasif20@gmail.com>
2023-04-20 18:34:47 +0500

    Updated wizard.py

--- enemies/wizard.py
+++ enemies/wizard.py
@@ -3,11 +3,14 @@ import os
 from .enemy import Enemy
 
 class Wizard(Enemy):
-    images = []
 
-    for x in range (20):
-        add_str = str(x)
-        if x < 10:
-            add_str = "0" + add_str
-        images.append(pygame.transform.scale(pygame.image.load(os.path.join("assets/enemies/PNG/2", "2_enemies_1_run_0" + add_str + ".png")), (64, 64) ))
+    def __init__(self):
+        super().__init__()
+        self.images = []
+
+        for x in range (20):
+            add_str = str(x)
+            if x < 10:
+                add_str = "0" + add_str
+            self.images.append(pygame.transform.scale(pygame.image.load(os.path.join("assets/enemies/PNG/2", "2_enemies_1_run_0" + add_str + ".png")), (64, 64) ))
             
