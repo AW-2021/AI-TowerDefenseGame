a4dcb6ca68ad56309cfaf5f246ebe9402ae5a769
Amina Wasif <aminawasif20@gmail.com>
2023-05-01 17:57:09 +0500

    Created tower.py in towers folder

---
+++ towers/tower.py
@@ -0,0 +1,28 @@
+
+class Tower:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 0
+        self.height = 0
+        self.sellCost = [0,0,0]
+        self.cost = [0,0,0]
+        self.level = 1
+        self.selected = False
+        self.menu = None
+        self.images = []
+
+    def draw(self, win):
+        pass
+
+    def click(self):
+        pass
+
+    def sell(self):
+        pass
+
+    def upgrade(self):
+        pass
+    
+    def move(self):
+        pass
