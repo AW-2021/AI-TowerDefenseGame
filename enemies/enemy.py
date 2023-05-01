4fef7d40fad60edd910e5decbfd78ee428a36231
Amina Wasif <aminawasif20@gmail.com>
2023-05-01 16:11:44 +0500

    Fixed movement of enemy across map

--- enemies/enemy.py
+++ enemies/enemy.py
@@ -9,7 +9,7 @@ class Enemy:
         self.animation_count = 0
         self.health = 1
         self.velocity = 3
-        self.path = [(19, 224), (177, 235), (282, 283), (526, 277), (607, 217), (641, 105), (717, 57), (796, 83), (855, 222), (973, 284), (1046, 366), (1022, 458), (894, 492), (740, 504), (580, 542), (148, 541), (85, 452), (52, 345), (1, 335), (-20, 355)] # List of points that define the path taken (currently hardcoded). Replace with path-finding algorithm
+        self.path = [(-10, 224), (1, 224), (177, 235), (282, 283), (526, 277), (607, 217), (641, 105), (717, 57), (796, 83), (855, 222), (973, 284), (1046, 366), (1022, 458), (894, 492), (740, 504), (580, 542), (148, 541), (85, 452), (52, 345), (1, 335), (-20, 355)] # List of points that define the path taken (currently hardcoded). Replace with path-finding algorithm
         self.x = self.path[0][0]
         self.y = self.path[0][1]
         self.img = None
@@ -30,7 +30,7 @@ class Enemy:
         if self.animation_count >= len(self.images):
             self.animation_count = 0
 
-        win.blit(self.img, (self.x, self.y))
+        win.blit(self.img, (self.x - self.img.get_width()/2, self.y - self.img.get_height()/2 - 20))
         self.move()
 
     def collide(self, X, Y):
@@ -50,36 +50,42 @@ class Enemy:
         else:
             x2, y2 = self.path[self.pathPos + 1]
 
-        # Total distance needed to move to reach destination pt
-        moveDist = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-
-        self.moveCount += 1
-        direction = (x2-x1, y2-y1)
+        direction = ((x2-x1) * 2, (y2-y1) * 2)
+        length = math.sqrt((direction[0]**2) + (direction[1]**2))
+        direction = (direction[0]/length, direction[1]/length) # Unit vector
 
         # Flipping image horizontally when the enemy turns around on path (-x direction)
         if direction[0] < 0 and not(self.flipped):
             self.flipped = True
             for x, img in enumerate(self.images):
                 self.images[x] = pygame.transform.flip(img, True, False)
-                
 
         # Calculating how much distance moved from current point to next
-        x_move, y_move = ((self.x + direction[0] * self.moveCount), (self.y + direction[1] * self.moveCount))
-        self.dist += math.sqrt((x_move - x1) ** 2 + (y_move - y1) ** 2)
-        print(self.dist)
+        x_move, y_move = ((self.x + direction[0]), (self.y + direction[1]))
 
         self.x = x_move
         self.y = y_move
         
-        # Go to next point
-        if self.dist >= moveDist:
-            self.dist = 0
-            self.moveCount = 0
-            self.pathPos += 1
-            if self.pathPos >= len(self.path):
-                return False
+        # Go to the next point
+        if direction[0] >= 0: # Moving right
+            if direction[1] >= 0: # Moving downwards
+                if self.x >= x2 and self.y >= y2:
+                    self.pathPos += 1
+
+            else: # Moving upwards
+                if self.x >= x2 and self.y <= y2:
+                    self.pathPos += 1
         
-        return True
+        else: # Moving left
+            if direction[1] >= 0: # Moving downwards
+                if self.x <= x2 and self.y >= y2:
+                    self.pathPos += 1
+
+            else: # Moving upwards
+                if self.x <= x2 and self.y <= y2:
+                    self.pathPos += 1
+        
+     
 
     def hit(self):
         # Returns if an enemy has died and removes one health
